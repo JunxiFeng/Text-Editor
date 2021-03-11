79371ca42cd9d5c206dd15c23e47c41f70eeb089
SnowWalk-Alpha <60299660+SnowWalk-Alpha@users.noreply.github.com>
2021-03-11 20:54:08 +0800

    Update StudentSpellCheck.cpp

--- StudentSpellCheck.cpp
+++ StudentSpellCheck.cpp
@@ -5,18 +5,18 @@
 #include <fstream>
 SpellCheck* createSpellCheck()
 {
-	return new StudentSpellCheck;
+    return new StudentSpellCheck;
 }
 
 StudentSpellCheck::~StudentSpellCheck()
 {
-	m_trie->Free(m_trie->helper());
+    m_trie->Free(m_trie->helper());
     delete m_trie;// TODO
 }
 
 bool StudentSpellCheck::load(std::string dictionaryFile)
 {
-	std::ifstream infile(dictionaryFile);
+    std::ifstream infile(dictionaryFile);
     if(!infile)
     {
         return false;
@@ -32,18 +32,81 @@ bool StudentSpellCheck::load(std::string dictionaryFile)
 
 bool StudentSpellCheck::spellCheck(std::string word, int max_suggestions, std::vector<std::string>& suggestions)
 {
-    /*
-    Node* curr=m_trie->helper();
-    int count=0;
-    for(int i=0;i<=27;i++)
+    Node *curr=m_trie->helper();//Point to the root of the trie
+    word=word+".";
+    bool check=true;
+    for(int i=0;i<word.size();i++)
     {
-        curr=curr->children[i];
-        if(curr->letter!=word[i])
-            count++;
+        char a=word[i];
+        int index=word[i]-97;
+        if(index<0) //Upper case
+            index=a-65;
+        if(word[i]=='\'')
+            index=26;
+        if(word[i]=='.')
+            index=27;
+        if(curr->children[index]==nullptr)
+        {
+            std:: vector<std::string>:: iterator it=suggestions.begin();
+            while(it!=suggestions.end())
+                it=suggestions.erase(it);
+            check=false;
+            break;
+        }
+        else
+            curr=curr->children[index];
     }
-	return false;
-     */
-    return false;
+    
+     if(check == false)
+     {
+        std::string temp; //Make a copy of the word passed in
+        for(int i =0; i< word.size(); i++)
+        {
+            temp=word;
+            for(int j =0; j<27; j++)
+            {
+                if(j==26)
+                    temp[i]='\'';
+                else
+                temp[i] = 'a'+j;//Each position of the word changes accordingly
+                Node * theNode = m_trie->helper();
+                bool check2 = true;
+                
+                for(int k=0; k < word.size(); k++)
+                {
+                    char b = temp[k];
+                    int index2 = b-97;
+                    if(index2 <0) //Upper Lower case conversion
+                        index2 = b-65;
+                    if(b == '\'')
+                        index2 = 26;
+                    if(b == '.')
+                        index2 = 27;
+                    //if not found
+                    if(theNode->children[index2] == nullptr)
+                    {
+                        check2 = false;
+                        break;
+                    }
+                    else
+                        theNode = theNode->children[index2];
+                }
+                
+                if(check2)
+                {
+                    if(suggestions.size() < max_suggestions)
+                    {
+                    temp = temp.substr(0,temp.size()-1);
+                    transform(temp.begin(), temp.end(), temp.begin(), ::tolower);
+                    suggestions.push_back(temp);
+                    temp = word;
+                    }
+                }
+            }
+        }
+        }
+        return check;
+        
 }
 
 void StudentSpellCheck::spellCheckLine(const std::string& line, std::vector<SpellCheck::Position>& problems)
@@ -56,7 +119,7 @@ void StudentSpellCheck::spellCheckLine(const std::string& line, std::vector<Spel
     int count=0;
     Position m_pos;
     std::string temp;
-    for(int i=0;i<line.size();i++) //Loop throught the line
+    for(int i=0;i<line.size();i++) //Loop through the line
     {
         if(isalpha(line[i]))
         {
@@ -77,6 +140,14 @@ void StudentSpellCheck::spellCheckLine(const std::string& line, std::vector<Spel
         {
             m_pos.start=count;
             m_pos.end=i-1;
+            index.push_back(m_pos);
+            container.push_back(temp);
+            temp="";
+        }
+        if(i==line.size()-1 && temp.size())
+        {
+            m_pos.start=count;
+            m_pos.end=i;
             index.push_back(m_pos);
             container.push_back(temp);
             temp="";
